d4cafbca0c7c83adb3726507ce38b331d002c80e
Yakir Adela <yakirade@gmail.com>
2025-08-02 16:29:18 +0300

    Update upload-to-s3.tf

--- asterra/terraform/upload-to-s3.tf
+++ asterra/terraform/upload-to-s3.tf
@@ -2,11 +2,6 @@ resource "aws_s3_bucket" "iac_storage" {
   bucket = var.iac_storage_bucket
 }
 
-resource "aws_s3_bucket_acl" "iac_storage_acl" {
-  bucket = aws_s3_bucket.iac_storage.id
-  acl    = "private"
-}
-
 resource "aws_s3_object" "terraform" {
   bucket = aws_s3_bucket.iac_storage.bucket
   key    = "terraform.zip"
