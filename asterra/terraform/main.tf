58a05bcc3df57e73885165e406ae18cbaa19bd5a
Yakir Adela <yakirade@gmail.com>
2025-08-02 15:26:05 +0300

    Update main.tf

--- asterra/terraform/main.tf
+++ asterra/terraform/main.tf
@@ -31,7 +31,7 @@ resource "aws_db_instance" "postgres" {
     engine                  = "postgres"
     engine_version          = "13.4"
     instance_class          = 20
-    name                    = var.db_name
+    identifier              = var.db_name
     username                = var.db_username
     password                = var.db_password  
     vpc_security_group_ids  = [aws_security_group.rdp_sg.id]
