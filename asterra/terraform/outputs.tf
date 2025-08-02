825251bb42ff772093013863213374c39f1ecd35
Yakir Adela <yakirade@gmail.com>
2025-08-02 16:48:25 +0300

    Update outputs.tf

--- asterra/terraform/outputs.tf
+++ asterra/terraform/outputs.tf
@@ -1,11 +1,17 @@
-output "rds_endpoint" {
-  value = aws_db_instance.postgres.endpoint
+output "rds_cluster_endpoint" {
+  value       = aws_rds_cluster.postgres_cluster.endpoint
+  description = "The primary endpoint of the Aurora PostgreSQL cluster"
+}
+
+output "rds_cluster_reader_endpoint" {
+  value       = aws_rds_cluster.postgres_cluster.reader_endpoint
+  description = "The reader endpoint of the Aurora PostgreSQL cluster"
 }
 
 output "s3_bucket_name" {
-  value = aws_s3_bucket.geojson_bucket.bucket
+  value       = aws_s3_bucket.geojson_bucket.bucket
 }
 
 output "aws_ecr_repository_url" {
-  value = aws_ecr_repository.ecr-repo.repository_url
+  value       = aws_ecr_repository.ecr-repo.repository_url
 }
